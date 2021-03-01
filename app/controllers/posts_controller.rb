0bcc3dbe832835e26fd70d8ec0f052676cb12f6d
gusbikos <gusbikos@gmail.com>
2021-03-01 10:25:03 -0500

    commit tree a69ab1b4e906bd169731836bbdfcd54e21fa9d8c

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,18 +12,27 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
+	  #@post = Post.new(params.require(:post).permit(:title, :description)) - strong params long way with identifying
+	  #each key
+	  @post = Post.new(post_params)
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
+	  #@post.update(params.require(:post).permit(:title)) - long way 
+	  @post.update(post_params)
 	  redirect_to post_path(@post)
 	end
 
 	def edit
 	  @post = Post.find(params[:id])
+	end
+
+	private 
+
+	def post_params # - this private method is required when you use strong params 
+		params.require(:post).permit(:title, :description)
 	end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module BlogFlash
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.action_controller.permit_all_parameters = true
+    #config.action_controller.permit_all_parameters = true  - this line enables all params to be able to be manipulated
+    #and you can hack a website this way.
   end
 end
